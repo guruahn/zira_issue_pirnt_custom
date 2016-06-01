b12cf782cfa1a6a56177d8bdf3d75aca4619f475
guruahn <guruahn@gmail.com>
2016-06-01 14:22:52 +0900

    티켓 클릭시 삭제되도록.

--- content.js
+++ content.js
@@ -40,7 +40,9 @@ var title_size_config = {
 }
 function changeStyle(){
 
+	//지라 프린트화면이 아닌곳에서 실행되지 않도록
 	if( document.getElementById("jira") ){
+		
 		chrome.storage.sync.get({
 			favoriteColor: 'red',
 			titleSize: 'l',
@@ -50,44 +52,45 @@ function changeStyle(){
 			var ghx_card = document.getElementsByClassName('ghx-card');
 			//console.log("ghx_card.length: "+ghx_card);
 			for(var i=0, len=ghx_card.length; i<len; i++)
-	    {
-	    	//console.log("#" + items.favoriteColor);
-	    	//console.log(items.titleSize + "rem");
-        ghx_card[i].style.borderColor = "#" + items.favoriteColor;
+		    {
+		    	//console.log("#" + items.favoriteColor);
+		    	//console.log(items.titleSize + "rem");
+		        ghx_card[i].style.borderColor = "#" + items.favoriteColor;
 
-        //title setting
-        var ghx_card_summary = ghx_card[i].getElementsByClassName("ghx-card-summary")[0];
-        //console.log(title_size_config[items.titleSize].fontSize);
-        ghx_card_summary.style.fontSize = title_size_config[items.titleSize].fontSize;
-        ghx_card_summary.style.maxHeight = title_size_config[items.titleSize].maxHeight;
-        ghx_card_summary.style.margin = title_size_config[items.titleSize].margin;
-        ghx_card[i].style.height = title_size_config[items.titleSize].itemHeight;
+		        //title setting
+		        var ghx_card_summary = ghx_card[i].getElementsByClassName("ghx-card-summary")[0];
+		        //console.log(title_size_config[items.titleSize].fontSize);
+		        ghx_card_summary.style.fontSize = title_size_config[items.titleSize].fontSize;
+		        ghx_card_summary.style.maxHeight = title_size_config[items.titleSize].maxHeight;
+		        ghx_card_summary.style.margin = title_size_config[items.titleSize].margin;
+		        ghx_card[i].style.height = title_size_config[items.titleSize].itemHeight;
 
-        //클릭시 제거되는 이벤트 걸기
-        ghx_card[i].addEventListener("click", function(e) {
-	        //var cardWrap = e.target.parentElement;
-    			//(cardWrap).parentNode.removeChild(e.target);
-		    });
-	    }
+		        //클릭시 제거되는 이벤트 걸기
+		        ghx_card[i].addEventListener("click", deleteCard.bind(null, ghx_card[i]), false);
+		    }
 
-	    //ticket type setting
-	    var body_class_name = "ghx-print-card-body";
-	   	if( body_class_name.indexOf("ghx-print-" + items.ticketType)  == -1 ){
-	   		document.getElementById("jira").className = body_class_name + " ghx-print-" + items.ticketType;
-	   	}
+		    //ticket type setting
+		    var body_class_name = "ghx-print-card-body";
+		   	if( body_class_name.indexOf("ghx-print-" + items.ticketType)  == -1 ){
+		   		document.getElementById("jira").className = body_class_name + " ghx-print-" + items.ticketType;
+		   	}
 
-	   	if( items.ticketType == 'empty'){
-	   		var ghx_card_key = document.getElementsByClassName('ghx-card-key');
-	   		for(var i=0, len=ghx_card_key.length; i<len; i++){
-	   			var key = ghx_card_key[i].innerText;
-	   			split_key = key.split("-");
-	   			ghx_card_key[i].innerText = split_key[0] + "-";
-	   		}
-	   	}
+		   	if( items.ticketType == 'empty'){
+		   		var ghx_card_key = document.getElementsByClassName('ghx-card-key');
+		   		for(var i=0, len=ghx_card_key.length; i<len; i++){
+		   			var key = ghx_card_key[i].innerText;
+		   			split_key = key.split("-");
+		   			ghx_card_key[i].innerText = split_key[0] + "-";
+		   		}
+		   	}
 	    
 
 			
 		});
 	}
 	
+}
+
+function deleteCard(currentElement) {
+	currentElement.style.display = 'none';
 }
